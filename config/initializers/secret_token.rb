e38adb238173c559a517c618e095f271348828e2
OkayHughes <OkayHughes@gmail.com>
2014-04-17 19:10:40 -0400

    Add basic ruby, add basic heroku support

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoSkillz::Application.config.secret_key_base = '79ff01443d9f6dc3dee61bebdf3e56e30157706a786cb7960d00e1c8b4f6c3f6580d0ab3af65c6d1320d63fbb2983543a609c190344b59f488856af08e85efcc'
